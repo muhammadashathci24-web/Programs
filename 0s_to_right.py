59d7f8c1fd67466b2dc790a454eb48df0212554e
Muhammad Ashath <muhammad.ashath.ci24@sahyadri.edu.in>
2026-09-22 23:22:21 +0530

    Add files via upload

---
+++ 0s_to_right.py
@@ -0,0 +1,10 @@
+nums = list(map(int,input().split()))
+    
+x =0
+
+for i in range(0,len(nums)):
+    if nums[i]!=0:
+        nums[x], nums[i] = nums[i], nums[x]
+        x+=1
+        
+print(nums)
